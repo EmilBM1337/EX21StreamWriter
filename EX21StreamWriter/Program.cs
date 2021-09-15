a9bebfb9806cb40ca319c7b6760574f37598d136
emil41m5 <emil41m5@CV-BB-5340>
2021-09-15 14:47:18 +0200

    Add project files.

---
+++ EX21StreamWriter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EX21StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter("MyFile.txt"))
+            {
+                writer.Write("Dette bliver skrevet på en linje");
+                writer.Write(".. og det her fortsætter på samme linje");
+                writer.WriteLine(".. det her står på samme linje, men afsluttes med nyt linjeskift");
+                writer.WriteLine("Forskellen på Write og WriteLine er altså om den bliver afsluttet med nyt linjeskift eller ej");
+                writer.WriteLine("1");
+                writer.WriteLine("2");
+            }
+        }
+    }
+}
